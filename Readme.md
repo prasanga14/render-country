507479d6a91b85929394a0189cd035db79bf2618
Prasanga <prasangaghimire14@gmail.com>
2023-08-29 07:49:58 +0545

    handle error and add readme

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# install all node modules using
+
+npm i
+
+# to start the server, open terminal in project folder and run,
+
+nodemon index.js
+
+# if you dont have nodemon you can do
+
+node index.js
--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import axios from 'axios';
+import { render } from 'ejs';
 
+// creating app using express
 const app = express();
 const port = 3000;
 
+// middleware to access the public folder to access static files
 app.use(express.static('public'));
 
+// middleware to get the submitted data from form at index.ejs in urlencoded(object) form
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// display the frontend using ejs
 app.get('/', (req, res) => res.render('index.ejs'));
 
+// get and send the obtained data from api to index.ejs
 app.post('/submit', async (req, res) => {
   try {
+    // using API to get data
     const response = await axios.get(
       `https://restcountries.com/v3.1/name/${req.body.countryName}`
     );
     const [data] = response.data;
+    console.log(data);
+    // sending data to index.ejs
     res.render('index.ejs', {
       image: data.flags.png,
       name: data.name.common,
@@ -24,9 +33,14 @@ app.post('/submit', async (req, res) => {
       border: data.borders[0],
       continent: data.continents,
     });
+    // since if user hit wrong country throw the error
+    if (data === undefined) throw new Error();
   } catch (error) {
-    console.error(error);
+    console.log(error.message);
+    // catching and sending the error
+    res.render('index.ejs', { err: error.message });
   }
 });
 
+// run server at port 3000
 app.listen(port, () => console.log(`Server running at ${port}`));
